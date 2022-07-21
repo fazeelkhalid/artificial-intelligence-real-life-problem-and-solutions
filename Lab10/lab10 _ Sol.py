d7998fbf20923965c3153ef9f7eb8a42fd19ae5a
Fazeel Khalid <fazeelkhalid9@gmail.com>
2022-07-21 23:16:16 +0500

    Lab10 solutions

---
+++ Lab10/lab10 _ Sol.py
@@ -0,0 +1,33 @@
+# q2
+from sklearn.cluster import KMeans
+import pandas as pd
+from sklearn.cluster import AgglomerativeClustering
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
+df = pd.read_excel("OnlineRetail (1).xlsx")
+df.head(5)
+df = df.drop(columns=['StockCode', 'InvoiceNo',
+             'InvoiceDate', 'Description', 'Country'], axis=1)
+df = df.dropna()
+cluster = AgglomerativeClustering(
+    n_clusters=1, affinity='euclidean', linkage='ward')
+cluster.fit_predict(df)
+plt.figure(figsize=(8, 8))
+plt.scatter(df['Quantity'], df['UnitPrice'], c=cluster.labels_)
+plt.show()
+
+
+# Q1
+%matplotlib inline
+df = pd.read_excel("OnlineRetail (1).xlsx")
+df.head(5)
+df = df.drop(columns=['StockCode', 'InvoiceNo',
+             'InvoiceDate', 'Description', 'Country'], axis=1)
+df = df.dropna()
+kmeans = KMeans(n_clusters=4, max_iter=50)
+labels = kmeans.fit(df)
+plt.figure(figsize=(8, 8))
+plt.scatter(df['Quantity'], df['UnitPrice'], c=kmeans.labels_)
+plt.show()
