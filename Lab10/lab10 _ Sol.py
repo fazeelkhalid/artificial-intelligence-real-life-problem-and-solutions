a8830eea7e1ba43b9db77a8c8deb7316561b332a
Fazeel Khalid <fazeelkhalid9@gmail.com>
2022-08-06 21:29:32 +0500

    Update lab10 _ Sol.py

--- Lab10/lab10 _ Sol.py
+++ Lab10/lab10 _ Sol.py
@@ -18,7 +18,7 @@ plt.figure(figsize=(8, 8))
 plt.scatter(df['Quantity'], df['UnitPrice'], c=cluster.labels_)
 plt.show()
 
-
+#------------------------------------------------------------------------
 # Question1
 %matplotlib inline # comment it if you are not using jupyter notebook
 df = pd.read_excel("OnlineRetail (1).xlsx")
